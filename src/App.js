0fca905ec583737416ee13bcd12b4acf2aab82cf
sofiree <muosophia0@gmail.com>
2020-10-12 19:29:26 +0100

    import components from components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import logo from './logo.svg';
+// import './App.css';
+import NavMain from './components/NavMain';
+import Landingpage from './components/Landingpage';
+import Navhome from './components/Navhome';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Softwaredeveloper from './components/Softwaredeveloper';
+import Productdesigner from './components/Productdesigner';
+import Apply from './components/Apply';
+import Journal from './components/Journal';
+import FAQ from './components/FAQ';
+
+
+
+
+
 
 export default App;
