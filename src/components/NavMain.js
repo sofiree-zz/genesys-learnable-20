a8bbf78629624012be976d4892e6512e7449fb41
sofiree <muosophia0@gmail.com>
2020-10-13 01:45:24 +0100

    add header logo and img removed from  landing page

--- src/components/NavMain.js
+++ src/components/NavMain.js
@@ -4,14 +4,19 @@ import {Link, withRouter} from 'react-router-dom';
 const Navigation = () => {
     return(
         <div className="header">
-            <nav>
-                <Link class="learnhome" to = "/Learnable">Learnable</Link>
-                <Link class="agora" to="/">Agora</Link>
-                <Link class="startzone" to="/">StartZone</Link>
-                <Link class="about" to="/">About</Link>
-                <Link class="contact" to="/">Contact</Link>
-                <Link class="devstudio" to="/">Dev Studio</Link>
-            </nav> 
+            {/* header for mobile */}
+         <img id="logo_" src="src/logo.svg" />
+
+         {/* this image here is supposed to pop ou the nav below....??? toogle or something...*/}
+            <img id="menu_" src="src/images/menu_24px.svg"/>
+                <nav>
+                    <Link class="learnhome" to = "/Learnable">Learnable</Link>
+                    <Link class="agora" to="/">Agora</Link>
+                    <Link class="startzone" to="/">StartZone</Link>
+                    <Link class="about" to="/">About</Link>
+                    <Link class="contact" to="/">Contact</Link>
+                    <Link class="devstudio" to="/">Dev Studio</Link>
+                </nav>       
         </div>
     );
 }
