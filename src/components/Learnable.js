9d5e3ccc9e1275bd397dc74473d5a7de95598b1c
sofiree <muosophia0@gmail.com>
2020-10-13 01:47:13 +0100

    create Learnable homepage

---
+++ src/components/Learnable.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavMain from './NavMain';
+import Navhome from './Navhome';
+
+const learnableHomepage=()=>{
+    return(
+        <div>
+            <NavMain/>
+            <Navhome/>
+
+        </div>
+    );
+}
+
+export default learnableHomepage;
