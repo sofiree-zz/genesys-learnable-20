55943c680aee5bb56c0b61a191b6e09659929879
sofiree <muosophia0@gmail.com>
2020-10-15 16:29:07 +0100

    create arrow up component

---
+++ src/components/Arrowup.js
@@ -0,0 +1,21 @@
+import React, {Components} from 'react';
+
+import Up from "./images/arrowup.svg";
+
+
+
+
+class Arrowsup extends React.Component{
+    render(){
+        return(
+            <div className="arr_con1">
+                {/* arrow taking you back up the page */}
+                <button className="up_arrow">
+                    <img src={Up}/> 
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Arrowsup;
