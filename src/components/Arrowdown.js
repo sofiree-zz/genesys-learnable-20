64c1918d10de094ff5c6b2277c734a45fd07284a
sofiree <muosophia0@gmail.com>
2020-10-15 16:27:08 +0100

    create arrow down components

---
+++ src/components/Arrowdown.js
@@ -0,0 +1,21 @@
+import React, {Components} from 'react';
+import Down from "./images/arrowdown.svg";
+
+
+
+
+
+class Arrowsdown extends React.Component{
+    render(){
+        return(
+            <div className="arr_con">
+                {/* arrow taking you down to end of page */}
+                <button className="down_arrow">
+                    <img src={Down}/>
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Arrowsdown;
