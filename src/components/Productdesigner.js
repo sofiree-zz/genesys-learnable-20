bf02c8fa0f34aee65d1b8cef837e61287e7167d1
sofiree <muosophia0@gmail.com>
2020-10-16 21:16:33 +0100

    Softwaredeveloper and Productdesigner css style

--- src/components/Productdesigner.js
+++ src/components/Productdesigner.js
@@ -4,78 +4,95 @@ import Cutout from "./images/Cutout 1.svg";
 import Designer from "./images/designer.png";
 import Subscribe from "./Subscribe";
 import Footer from './Footer';
+import Arrowsup from './Arrowup';
 
 class Designers extends React.Component{
     render(){
         return(
             <div>
-                <section className="content2">
-                    <div className="des_con1">
-                        <h2>
-                            The<br/>
-                            Learnable<br/>
-                            Product Designer
-                        </h2>
-                        <div className="cut_out2">
-                            <img src={Cutout}/>
+                <section className="hood" >
+                    <div className="content">
+                        <div className="dev_con1">
+                            <div className="dev_note">
+                                <h5>
+                                    The<br/>
+                                    Learnable<br/>
+                                    Product Designer
+                                </h5>
+
+                                <p>
+                                    At Genesys, you'll be exposed to every discipline in our creative<br/>
+                                    process – UX, UI, interaction design, and copywriting. You’ll be<br/>
+                                    part of a dynamic team that's made up of like-minded, talented<br/>
+                                    people. You'll work hard with them to create things that will<br/>
+                                    amaze everyone everywhere.<br/>
+                                    <br/>
+                                    In addition to your team projects, you’ll be lending a hand to the<br/>
+                                    creative team. Participating in brainstorm sessions, building<br/>
+                                    winning pitches, preparing assets, and design scriptures for<br/>
+                                    productions, you'll be in the thick of it all.<br/>
+                                    <br/>
+                                    Oh, and you’ll make amazing friends while you're at it!               
+                                </p>
+                            </div>
+
+                            <div className="cut_out2">
+                                <img src={Cutout}/>
+                            </div>
                         </div>
+                        <div className="quote1">
+                            <q>
+                                “The greats weren’t great<br/>
+                                because at birth they could<br/>
+                                paint. The greats were<br/>
+                                great because they painted a lot.” <br/>
+                                - Macklemore
+                            </q> 
+                        </div>
+
                         
-                        <p>
-                            At Genesys, you'll be exposed to every discipline in our creative<br/>
-                            process – UX, UI, interaction design, and copywriting. You’ll be<br/>
-                            part of a dynamic team that's made up of like-minded, talented<br/>
-                            people. You'll work hard with them to create things that will<br/>
-                            amaze everyone everywhere.<br/>
-                            <br/>
-                            In addition to your team projects, you’ll be lending a hand to the<br/>
-                            creative team. Participating in brainstorm sessions, building<br/>
-                            winning pitches, preparing assets, and design scriptures for<br/>
-                            productions, you'll be in the thick of it all.<br/>
-                            <br/>
-                            Oh, and you’ll make amazing friends while you're at it!               
-                        </p>
 
+                        
+                            <div  className="des_">
+                                    <img src={Designer}/>
+                            </div>
+
+                            <div className="dev_note_3">
+                                <h6>We expect you to...</h6>
+                                
+                                <div className="p1">
+                                    <p >
+                                        Play nice with other people.<br/>
+                                        <br/>
+                                        Be an actively engaging person. That means you are present,<br/>
+                                        motivated, and social.<br/>
+                                        <br/>
+                                        Understand how to use Figma design software<br/>
+                                        <br/>
+                                        Be able to create initial design layouts with confidence and<br/>
+                                        know-how to effectively communicate concepts to everyone on<br/>
+                                        your team.<br/>
+                                        <br/>
+                                        Have a basic understanding of design principles and theories<br/>
+                                        To have courage. This is a tough industry, only the brave<br/>
+                                        and sturdy remain.<br/>
+                                        <br/>
+                                        Be prepared to defend your decisions and take constructive<br/>
+                                        criticism.
+                                    </p>
+                                </div>
+
+                                <div className="p2">
+                                    <p >
+                                        To us, beauty without substance has no value. At Genesys, design thinkin and<br/>
+                                        execution are equally important, so we're constantly on the lookout for not only the<br/>
+                                        how but also the why.
+                                    </p>
+                                </div>            
+                            </div>                                       
                     </div>
-                   
-                    <q>
-                    “The greats weren’t great<br/>
-                    because at birth they could<br/>
-                    paint. The greats were<br/>
-                    great because they painted a lot.” <br/>
-                    - Macklemore
-                    </q>
-
-                    <div className="des_con2">
-                        <div  className="des_">
-                            <img src={Designer}/>
-                        </div>
-                        <h6>We expect you to...</h6>
-                        <p className="p1">
-                            Play nice with other people.<br/>
-                            <br/>
-                            Be an actively engaging person. That means you are present,<br/>
-                            motivated, and social.<br/>
-                            <br/>
-                            Understand how to use Figma design software<br/>
-                            <br/>
-                            Be able to create initial design layouts with confidence and<br/>
-                            know-how to effectively communicate concepts to everyone on<br/>
-                            your team.<br/>
-                            <br/>
-                            Have a basic understanding of design principles and theories<br/>
-                            To have courage. This is a tough industry, only the brave<br/>
-                            and sturdy remain.<br/>
-                            <br/>
-                            Be prepared to defend your decisions and take constructive<br/>
-                            criticism.
-                        </p>
-                        <p className="p2">
-                            To us, beauty without substance has no value. At Genesys, design thinkin and<br/>
-                            execution are equally important, so we're constantly on the lookout for not only the<br/>
-                            how but also the why.
-                        </p>
-                    </div>   
                 </section>
+                <Arrowsup/>
                 <Subscribe/>
             </div>
         );
--- src/components/Softwaredeveloper.js
+++ src/components/Softwaredeveloper.js
@@ -11,63 +11,77 @@ class Softwaredevs  extends React.Component{
     render(){
         return(
             <div>           
-                <section className="content">
-                    <div className="dev_con1">
-                        <h2>
-                            The<br/>
-                            Learnable<br/>
-                            Software<br/>
-                            developer
-                        </h2>
-                        <div className="cut_out1">
-                            <img  src={Cutout}/>
+                <section className="hood" >
+                    <div className="content">
+                        <div className="dev_con1">
+                            <div className="dev_note">
+                            <h5>
+                                The<br/>
+                                Learnable<br/>
+                                Software<br/>
+                                developer
+                            </h5>
+
+                            <p>
+                                Creativity means a lot to us. So we look for people who can think<br/>
+                                both logistically and artistically, and who know that limits are<br/>
+                                made to be pushed. We value skill and aptitude, but we honor<br/>
+                                those who care about working with their team to add the<br/>
+                                Genesys flavour to every project.<br/>
+                                <br/> 
+                                As a developer, being part of the team exposes you to a range of<br/>
+                                diverse, like-minded talented people who are sure to make the<br/>
+                                experience fun for you. You’ll get a shot at building amazing<br/>
+                                things, developing yourself, learning on the job, while<br/>
+                                exchanging healthy jokes and having random hangouts once in a while.
+                            </p>
+                            </div>
+                            
+
+                            <div className="cut_out1">
+                                <img  src={Cutout}/>
+                            </div>
+                        </div>
+                        
+                        <div className="quote">
+                            <q>
+                                You are someone people<br/>can count on. Every day,<br/> 
+                                you come in ready to make<br/>great things happen.
+                            </q>
                         </div>
 
-                        <p>
-                            Creativity means a lot to us. So we look for people who can think<br/>
-                            both logistically and artistically, and who know that limits are<br/>
-                            made to be pushed. We value skill and aptitude, but we honor<br/>
-                            those who care about working with their team to add the<br/>
-                            Genesys flavour to every project.<br/>
-                            <br/> 
-                            As a developer, being part of the team exposes you to a range of<br/>
-                            diverse, like-minded talented people who are sure to make the<br/>
-                            experience fun for you. You’ll get a shot at building amazing<br/>
-                            things, developing yourself, learning on the job, while<br/>
-                            exchanging healthy jokes and having random hangouts once in a while.
-                        </p>
+                        
 
+                        <div className="dev_con2">
+                            <div className="dev_" >
+                                <img src={Developer}/>
+                            </div>
+
+                            <div className="dev_note_2">
+                                <h6>We expect you to...</h6>
+                                <p>
+                                    Play nice with other people.<br/>
+                                    <br/>
+                                    Be an actively engaging person. That means you are present,<br/>
+                                    motivated, and social. You know how to communicate effectively<br/>
+                                    with your teammates. Love to code. You understand the basics of programming and<br/>
+                                    object-oriented design and development.<br/>
+                                    <br/>
+                                    Have an eye for detail. Your keen observation helps you catch<br/>
+                                    discrepancies and fix them quickly.<br/>
+                                    <br/>
+                                    Be innately curious. Your love for technology is insatiable, so<br/>
+                                    you’re always researching and experimenting. Learning new<br/>
+                                    technical skills while on the job is something you look forward to.<br/>
+                                    <br/>
+                                    Appreciate good design. You want to make something that<br/>
+                                    doesn't just work flawlessly but looks amazing.
+                                </p>
+                            </div>
+                            
+                        </div>
                     </div>
                     
-                    <q>
-                        “You are someone people<br/>can count on. Every day,<br/> 
-                        you come in ready to make<br/>great things happen.” 
-                    </q>
-
-                    <div className="dev_con2">
-                        <div className="dev_" >
-                            <img src={Developer}/>
-                        </div>
-                        <h6>We expect you to...</h6>
-                        <p>
-                            Play nice with other people.<br/>
-                            <br/>
-                            Be an actively engaging person. That means you are present,<br/>
-                            motivated, and social. You know how to communicate effectively<br/>
-                            with your teammates. Love to code. You understand the basics of programming and<br/>
-                            object-oriented design and development.<br/>
-                            <br/>
-                            Have an eye for detail. Your keen observation helps you catch<br/>
-                            discrepancies and fix them quickly.<br/>
-                            <br/>
-                            Be innately curious. Your love for technology is insatiable, so<br/>
-                            you’re always researching and experimenting. Learning new<br/>
-                            technical skills while on the job is something you look forward to.<br/>
-                            <br/>
-                            Appreciate good design. You want to make something that<br/>
-                            doesn't just work flawlessly but looks amazing.
-                        </p>
-                    </div>
                     
                 </section>
                 <Arrowsup/>
