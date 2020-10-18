df9233a2e883e6c2adebb9d8222b684983eb7c8f
sofiree <muosophia0@gmail.com>
2020-10-18 03:32:48 +0100

    journal update and styling new

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import NavMain from './NavMain';
 import NavHome from './NavHome';
-import Apply2 from "./images/apply2.png";
+import Apply3 from "./images/apply3.png";
+import DavidMong from "./images/davidmong.png";
 import JournalPic from "./images/journal.png";
 import Journal1 from "./images/journal1.png";
 import Journal2 from "./images/journal2.png";
@@ -16,12 +17,17 @@ class Journal extends React.Component{
             <div>
                 <div className="content4">
                     <section className="HJ">
+                        <div className="heading_">
                         <h1>Journal</h1>
+                        </div>
+                        <div className="button_">
                         <button>CATEGORIES</button>
+                        </div>
+                        
                     </section>
                     <section className="heroJ">
-                        <img className="app_3" src={Apply2}/>
-                        <div>
+                        <img className="app_3" src={Apply3}/>
+                        <div className="inspiration">
                             <a href="#">INSPIRATION</a>
                             <h3>Learn Design Thinking<br/>
                             like a Pro and Build<br/>
@@ -36,62 +42,65 @@ class Journal extends React.Component{
                                 dui porttitor cursus. Ipsum sem ut tortor ac ullamcorper.<br/>
                                 Lacus.
                             </p>
-                            <div className="david"><img/> <p>David Mong, Apr 9, 2020</p></div>
+                            <div className="david">
+                                <img src={DavidMong}/> 
+                            <p>David Mong, Apr 9, 2020</p></div>
                         </div>
                     </section>
 
                     <section className="journalofjournals">
-                        <div className="journals">
-                            <div>
+                        <div className="journals_">
+                            <div className="journ">
                                 <img src={JournalPic}/>
                                 <h6>INSPIRATION</h6>
                                 <h2>Learn Design Thinking like a Pro and Build Projects that people will care about</h2>
                                 <p>David Mong, Apr 9, 2020</p>
                             </div>
-                            <div>
+                            <div className="journ">
                                 <img src={Journal1}/>
                                 <h6>INSPIRATION</h6>
                                 <h2>Learn Design Thinking like a Pro and Build Projects that people will care about</h2>
                                 <p>David Mong, Apr 9, 2020</p>
                             </div>
-                            <div>
+                            <div className="journ"> 
                                 <img src={Journal2}/>
                                 <h6>INSPIRATION</h6>
                                 <h2>Learn Design Thinking like a Pro and Build Projects that people will care about</h2>
                                 <p>David Mong, Apr 9, 2020</p>
                             </div>
-                            <div>
+                            <div className="journ">
                                 <img src={Journal3}/>
                                 <h6>INSPIRATION</h6>
                                 <h2>Learn Design Thinking like a Pro and Build Projects that people will care about</h2>
                                 <p>David Mong, Apr 9, 2020</p>
                             </div>
-                            <div>
-                                <img src={Journal}/>
+                            <div className="journ">
+                                <img src={JournalPic}/>
                                 <h6>INSPIRATION</h6>
                                 <h2>Learn Design Thinking like a Pro and Build Projects that people will care about</h2>
                                 <p>David Mong, Apr 9, 2020</p>
                             </div>
-                            <div>
+                            <div className="journ">
                                 <img src={Journal1}/>
                                 <h6>INSPIRATION</h6>
                                 <h2>Learn Design Thinking like a Pro and Build Projects that people will care about</h2>
                                 <p>David Mong, Apr 9, 2020</p>
                             </div>
-                            <div>
+                            <div className="journ">
                                 <img src={Journal2}/>
                                 <h6>INSPIRATION</h6>
                                 <h2>Learn Design Thinking like a Pro and Build Projects that people will care about</h2>
                                 <p>David Mong, Apr 9, 2020</p>
                             </div>
-                            <div>
+                            <div className="journ">
                                 <img src={Journal3}/>
                                 <h6>INSPIRATION</h6>
                                 <h2>Learn Design Thinking like a Pro and Build Projects that people will care about</h2>
                                 <p>David Mong, Apr 9, 2020</p>
                             </div>
+                             
                         </div>
-                        <button>LOAD MORE</button>      
+                        <button className="loadbutton">LOAD MORE</button> 
                     </section>
 
                 </div>
