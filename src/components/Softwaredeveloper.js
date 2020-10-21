9a5aca27d868f712635ac6be7fe664c82a234b03
sofiree <muosophia0@gmail.com>
2020-10-21 11:02:53 +0100

    softwaredev page mediaquery 100%

--- src/components/Softwaredeveloper.js
+++ src/components/Softwaredeveloper.js
@@ -15,26 +15,23 @@ class SoftwareDeveloper  extends React.Component{
                     <div className="content">
                         <div className="dev_con1">
                             <div className="dev_note">
-                            <h5>
-                                The<br/>
-                                Learnable<br/>
-                                Software<br/>
-                                developer
-                            </h5>
+                                <h5>
+                                    The<br/> Learnable<br/> Software<br/> developer
+                                </h5>
 
-                            <p>
-                                Creativity means a lot to us. So we look for people who can think<br/>
-                                both logistically and artistically, and who know that limits are<br/>
-                                made to be pushed. We value skill and aptitude, but we honor<br/>
-                                those who care about working with their team to add the<br/>
-                                Genesys flavour to every project.<br/>
-                                <br/> 
-                                As a developer, being part of the team exposes you to a range of<br/>
-                                diverse, like-minded talented people who are sure to make the<br/>
-                                experience fun for you. You’ll get a shot at building amazing<br/>
-                                things, developing yourself, learning on the job, while<br/>
-                                exchanging healthy jokes and having random hangouts once in a while.
-                            </p>
+                                <p>
+                                    Creativity means a lot to us. So we look for people who can think<br/>
+                                    both logistically and artistically, and who know that limits are<br/>
+                                    made to be pushed. We value skill and aptitude, but we honor<br/>
+                                    those who care about working with their team to add the<br/>
+                                    Genesys flavour to every project.<br/>
+                                    <br/> 
+                                    As a developer, being part of the team exposes you to a range of<br/>
+                                    diverse, like-minded talented people who are sure to make the<br/>
+                                    experience fun for you. You’ll get a shot at building amazing<br/>
+                                    things, developing yourself, learning on the job, while<br/>
+                                    exchanging healthy jokes and having random hangouts once in a while.
+                                </p>
                             </div>
                             
 
